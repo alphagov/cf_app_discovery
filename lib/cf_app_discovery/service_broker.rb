1d086d660e559c83f78998394fde55ec7353a3e7
David King <david.king@digital.cabinet-office.gov.uk>
2018-03-09 17:26:01 +0000

    Add a more descriptive service catalog
    Links off to the RE docs for more info

--- lib/cf_app_discovery/service_broker.rb
+++ lib/cf_app_discovery/service_broker.rb
@@ -15,14 +15,14 @@ class CfAppDiscovery
         services: [
           {
             id: "fd609087-70e0-4c8c-8916-b6885ac156a3",
-            name: "prometheus",
-            description: "Test Prometheus service broker",
+            name: "gds-prometheus",
+            description: "GDS internal Prometheus monitoring alpha https://reliability-engineering.cloudapps.digital/#metrics",
             bindable: true,
             plans: [
               {
                 id: "b5998c91-d379-4df7-b329-11450f8459f1",
-                name: "public",
-                description: "something",
+                name: "prometheus",
+                description: "Monitor your apps using Prometheus",
                 free: true,
               },
             ],
