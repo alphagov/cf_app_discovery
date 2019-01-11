f7c7938c16d5f7f644458f58e05d98fa1ffe976a
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2019-01-11 09:15:56 +0000

    allow focusing on tests using `fit`
    this is really useful but for some reason isn't the default behaviour.
    
    tip grabbed from https://stackoverflow.com/a/22113489

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -44,6 +44,8 @@ RSpec.configure do |config|
   config.color = true
 
   config.include FactoryBot::Syntax::Methods
+  config.filter_run focus: true
+  config.run_all_when_everything_filtered = true
 
   config.before(:suite) do
     FactoryBot.find_definitions
